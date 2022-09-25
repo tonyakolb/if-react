2b1522ff4ce504330834d105516f9e0b94b179f4
tonyakolb <antoninakolb@gmail.com>
2022-09-25 17:51:50 +0300

    lesson-33 redone

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import HomeGuestLoves from '../HomeGuestLoves/HomeGuestLoves';
+import './App.css'
+import React from 'react'
+
+const App=()=>(
+    <>
+        <HomeGuestLoves/>
+    </>
+)
+
+export default App;
