3a8f52e4bcb107b09b19b41e3f0d8ea8b2203cb9
tonyakolb <antoninakolb@gmail.com>
2022-10-12 00:01:01 +0300

    lesson-34

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import HomeGuestLoves from '../HomeGuestLoves/HomeGuestLoves';
-import './App.css'
-import React from 'react'
+import './App.css';
+import React from 'react';
 
 const App=()=>(
     <>
+        <div className='homes-title-center'><h2 className='title'>Home Guest Loves</h2></div>
         <HomeGuestLoves/>
     </>
 )
